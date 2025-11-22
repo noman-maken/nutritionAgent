9e00b12ff9e63c8d6f45f732cf7e884c843e244d
Noman Maken <mnomanaslam6@gmail.com>
2025-11-22 22:03:54 +0500

    Initial

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -2,7 +2,7 @@ const baseUrl =
     typeof window !== "undefined"
         ? `${window.location.protocol}//${window.location.host}`
         : process.env.NODE_ENV === "production"
-            ? ""
+            ? "https://nutrition-agent-alpha.vercel.app"
             : "http://localhost:3000";
 
 export default baseUrl;
